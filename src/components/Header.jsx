789983e3a9c39cd80526a451866e1665b84e3c21
Gaurav Ruhela <gauravruhela@ip-192-168-1-113.ap-south-1.compute.internal>
2023-08-09 07:22:51 +0400

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='flex flex-row justify-center mt-10 text-7xl'>
+      <div className='relax'>relax</div>
+      <div className='dot'>.</div>
+      <div className='chill'>chill</div>
+      <div className='dot'>.</div>
+      <div className='code'>code</div>
+    </div>
   )
 }
 
